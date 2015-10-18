2c57348d5e716d70dc06e60d35dd1f5eb1f74a8a
Corinne Kelly <corinnemariekelly@gmail.com>
2015-10-18 14:27:20 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+//alerts, console, functions
+function shoutHello(myString) {
+	console.log(myString);
+	alert("Hello world!");
+}
+
+shoutHello("Help!");
+
+// for loop, variables, arrays, logic
+var colors = ["Teal", "Pink", "Green"];
+for (var i = 0; i < colors.length; i++) {
+console.log(colors[i]);
+}
+
+
+// when you type in 1+4 the console returns 5
+// also when you type in 
+function addNums(num1, num2) {
+var sum = num1 + num2;
+console.log(sum);
+}
+
+addNums(5, 6)
